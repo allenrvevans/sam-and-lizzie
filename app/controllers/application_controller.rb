77a4926433a239c3384a7b3a091c6bcc048b1457
Caro Evans <caroline.w.evans@gmail.com>
2020-01-23 18:38:31 +1000

    setting up landing page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,6 @@
 class ApplicationController < ActionController::Base
+
+  def landing_page
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  root to: "application#landing_page"
+
   get 'rsvp/:code', to: 'rsvps#show'
 
 end
