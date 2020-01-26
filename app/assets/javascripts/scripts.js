2e5740210f39610870964a0311bd2e41f339988d
Caro Evans <caroline.w.evans@gmail.com>
2020-01-26 14:43:12 +1000

    removed unused script

--- app/assets/javascripts/scripts.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function() {
-  var location = window.location.pathname;        
-   $("nav [href]").each(function() {
-    if (this.href == location) {
-    $(this).addClass("active");
-}  
