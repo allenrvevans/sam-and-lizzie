ee931bbf25e233a0057437e3eb46d0eef8ffab51
Allen Evans <allen@blaize.io>
2020-01-26 18:31:43 +0000

    Hook up to airtable

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,11 @@ Rails.application.routes.draw do
 
   root to: "application#landing_page"
 
-  get 'rsvp/:code', to: 'rsvps#show'
+  get 'rsvp/thanks', to: 'rsvps#thanks'
+  get 'rsvp/:code', to: 'rsvps#edit'
+  put 'rsvp/:code', to: 'rsvps#update'
 
-  resources :rsvps, only: [:edit, :update]
+  #resources :rsvps, only: [:edit, :update]
 
   get '/details', to: 'application#details'
   get '/sunday', to: 'application#sunday'
